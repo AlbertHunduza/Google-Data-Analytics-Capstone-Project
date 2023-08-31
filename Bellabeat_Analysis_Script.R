c02833bb55b46a334e881ac8f79fd0ef1918d1d8
AlbertHunduza <alberthunduza1@gmail.com>
2023-08-31 22:26:06 +0200

    daily_activity analysis

--- Bellabeat_Analysis_Script.R
+++ Bellabeat_Analysis_Script.R
@@ -1,3 +1,9 @@
+# Load the required library
+library(dplyr)
+library(ggplot2)
+library(corrplot)
+
+
 # Importing relevant datasets
 data_folder_path <- "Fitabase Data 4.12.16-5.12.16/" # sets path of folder containing files
 
@@ -59,3 +65,115 @@ hourly_exercises <- merge(hourly_intensities, hourly_steps, by = c("Id", "Activi
 
 # analyzing the daily_activity dataframe
 
+# Get the number of unique IDs/people in the "Id" column
+unique_id_count <- daily_activity %>%
+  distinct(Id) %>%
+  nrow()
+
+# Print the number of unique IDs/people
+print(unique_id_count)
+
+# Convert the "ActivityDate" column to Date format
+daily_activity %>%
+  mutate(ActivityDate = as.Date(ActivityDate, format = "%m/%d/%Y")) %>%
+  distinct(ActivityDate) %>%
+  summarise(unique_date_count = n())
+
+
+
+# Convert the "ActivityDate" column to Date format and group by Id
+result <- daily_activity %>%
+  mutate(ActivityDate = as.Date(ActivityDate, format = "%m/%d/%Y")) %>%
+  group_by(Id) %>%
+  summarise(unique_date_count = n_distinct(ActivityDate))
+
+# Set the option to display all rows
+options(dplyr.print_max = Inf)
+
+# Print the result
+print(result)
+
+# Calculate the average number of steps and distance per person
+average_per_person <- daily_activity %>%
+  group_by(Id) %>%
+  summarise(average_steps = mean(TotalSteps, na.rm = TRUE),
+            average_distance = mean(TotalDistance, na.rm = TRUE))
+
+# Print the result
+print(average_per_person)
+
+# Calculate the average number of steps and distance per person
+average_per_person <- daily_activity %>%
+  group_by(Id) %>%
+  summarise(average_steps = mean(TotalSteps, na.rm = TRUE),
+            average_distance = mean(TotalDistance, na.rm = TRUE))
+
+# Create a histogram for average steps
+ggplot(average_per_person, aes(x = average_steps)) +
+  geom_histogram(binwidth = 500) +
+  labs(title = "Histogram of Average Steps per Person",
+       x = "Average Steps",
+       y = "Frequency")
+
+# Create a histogram for average distance
+ggplot(average_per_person, aes(x = average_distance)) +
+  geom_histogram(binwidth = 1, fill = "blue", color = "black") +
+  labs(title = "Histogram of Average Distance per Person",
+       x = "Average Distance",
+       y = "Frequency")
+
+# Select the relevant columns
+selected_columns <- daily_activity %>%
+  select(TotalSteps, TotalDistance, Calories)
+
+# Calculate the correlation matrix
+correlation_matrix <- cor(selected_columns, use = "complete.obs")
+
+# Visualize the correlation matrix with a title
+corrplot(correlation_matrix, method = "shade", type = "upper",
+         tl.col = "black", tl.srt = 45)
+
+
+# Convert the "ActivityDate" column to Date format and add a new column for the day of the week
+daily_activity_with_day <- daily_activity %>%
+  mutate(ActivityDate = as.Date(ActivityDate, format = "%m/%d/%Y"),
+         DayOfWeek = weekdays(ActivityDate))
+
+# Group by day of the week and calculate the average distance
+average_distance_per_day <- daily_activity_with_day %>%
+  group_by(DayOfWeek) %>%
+  summarise(average_distance = mean(TotalDistance, na.rm = TRUE))
+
+# Order the days of the week
+day_order <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+average_distance_per_day$DayOfWeek <- factor(average_distance_per_day$DayOfWeek, levels = day_order)
+
+# Create a bar plot
+ggplot(average_distance_per_day, aes(x = DayOfWeek, y = average_distance, fill = DayOfWeek)) +
+  geom_bar(stat = "identity") +
+  labs(title = "Average Distance per Day of the Week",
+       x = "Day of the Week",
+       y = "Average Distance") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# Select the relevant columns
+selected_columns <- daily_activity %>%
+  select(VeryActiveDistance, ModeratelyActiveDistance, LightActiveDistance, Calories)
+
+# Calculate the correlation matrix
+correlation_matrix <- cor(selected_columns, use = "complete.obs")
+
+# Visualize the correlation matrix with a title
+corrplot(correlation_matrix, method = "color", type = "upper",
+         tl.col = "black", tl.srt = 45)
+
+# Select the relevant columns
+selected_columns <- daily_activity %>%
+  select(VeryActiveMinutes, FairlyActiveMinutes, LightlyActiveMinutes, SedentaryMinutes, Calories)
+
+# Calculate the correlation matrix
+correlation_matrix <- cor(selected_columns, use = "complete.obs")
+
+# Visualize the correlation matrix with a title
+corrplot(correlation_matrix, method = "color", type = "upper",
+         tl.col = "black", tl.srt = 45)
